ef96e9d7e1a154b176b8768ddff1ef993e27aa79
Robert Sampaio <robertfqsampaio@gmail.com>
2024-04-29 16:53:18 -0300

    :tada: first Java commit

---
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Este é um exemplo de <b>javadoc</b>
+ */
+public class OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("BAN-KAI!");
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 20;
+        long bigNumber = 100000;
+        double salaryDouble = 2000;
+        double salaryFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean trueBol = true;
+        boolean falseBol = false;
+        char character = '\u0041';
+
+        System.out.println("A idade do programador é " + age);
+        System.out.println(trueBol);
+        System.out.println("char: " + character);
+    }
+}
