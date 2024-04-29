6b9fec0d5c93f2274323467da3fc8aaef714c166
Robert Sampaio <robertfqsampaio@gmail.com>
2024-04-29 19:11:25 -0300

    :rocket: class 14 - primitive type pt04 - strings

--- src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
@@ -5,16 +5,18 @@ public class TiposPrimitivos {
         // int, double, float, char, byte, short, long, boolean
         int age = 20;
         long bigNumber = 100000;
-        double salaryDouble = 2000;
-        double salaryFloat = 2500;
+        double salaryDouble = 2000.0;
+        double salaryFloat = 2500.0F;
         byte ageByte = 10;
         short ageShort = 10;
         boolean trueBol = true;
         boolean falseBol = false;
         char character = '\u0041';
+        String nome = "Robert";
 
         System.out.println("A idade do programador é " + age);
         System.out.println(trueBol);
         System.out.println("char: " + character);
+        System.out.println("Oi, meu nome é " + nome);
     }
 }
